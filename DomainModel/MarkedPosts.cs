7764b2b0b76b9fda62d12b3ff513b22474572471
Kerem <keremunal@yahoo.com>
2017-05-07 19:48:01 +0200

    Added Domain Models for all tables

---
+++ DomainModel/MarkedPosts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class MarkedPosts
+    {
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+    }
+}
