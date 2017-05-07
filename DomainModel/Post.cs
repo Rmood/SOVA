44ae316ecc5dd257bfb64028ac89be7a3a48c4e9
Kerem <keremunal@yahoo.com>
2017-05-07 20:39:19 +0200

    Context and Domain Model corrected and updated

---
+++ DomainModel/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int Post_Type_Id { get; set; }
+        public int Parent_Id { get; set; }
+        public int Accepted_Answer_Id { get; set; }
+        public DateTime Creation_Date { get; set; }
+        public int Score { get; set; }
+        public string Body { get; set; }
+        public DateTime Closed_Date { get; set; }
+        public string Title { get; set; }
+        public int User_Id { get; set; }
+    }
+}
--- SovaDatabase/SovaContext.cs
+++ SovaDatabase/SovaContext.cs
@@ -10,10 +10,17 @@ namespace SovaDatabase
 {
     public class SovaContext : DbContext
     {
+        public DbSet<Post> Posts { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Posttype> Posttypes { get; set; }
-
+        public DbSet<History> History { get; set; }
+        public DbSet<MarkedPost> MarkedPosts { get; set; }
+        public DbSet<PostLink> PostLinks { get; set; }
+        public DbSet<SovaUser> SovaUsers { get; set; }
+        public DbSet<TagLink> TagLinks { get; set; }
+        public DbSet<User> Users { get; set; }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
@@ -29,6 +36,11 @@ namespace SovaDatabase
             modelBuilder.Entity<Comment>().Property(x => x.date).HasColumnName("creation_date");
             modelBuilder.Entity<Comment>().Property(x => x.userId).HasColumnName("user_id");
             modelBuilder.Entity<Posttype>().ToTable("posttype");
+            modelBuilder.Entity<History>().HasKey(c => new {c.PostId, c.Access});
+            modelBuilder.Entity<MarkedPost>().HasKey(c => new {c.UserId, c.PostId});
+            modelBuilder.Entity<PostLink>().HasKey(c => new {c.PostId, c.LinkedPost});
+            modelBuilder.Entity<TagLink>().HasKey(c => new {c.post_id, c.tag_id});
+            modelBuilder.Entity<SovaUser>().HasKey(c => c.UserId);
         }
     }
 }
